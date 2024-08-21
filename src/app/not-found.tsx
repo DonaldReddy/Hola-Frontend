e93f99ac41e66bcfc859ef068040283006e9c86e
Donald Reddy Indelu <donaldreddy2712@gmail.com>
2024-08-21 13:14:07 +0530

    initial update

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function notFound() {
+	return (
+		<div className="flex justify-center items-center min-h-svh text-3xl">
+			404
+		</div>
+	);
+}
+
+export default notFound;
